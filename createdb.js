0745f4bc102c8c602fa6bb3e55df0758900e338f
git-show <git-show>
2017-04-24 14:29:01 +0300

    36 first 5 min

--- createdb.js
+++ createdb.js
@@ -1,19 +1,22 @@
-var User = require('models/user').User;
+var mongoose = require('./libs/mongoose');
+var async = require('async');
+var User = require('./models/user').User;
 
-var user = new User({
-  username: 'Tester2',
-  password: 'secret'
+mongoose.connection.on('open', function() {
+  var db = mongoose.connection.db;
+  db.dropDatabase(function(err) {
+    if (err) {
+      throw err;
+    } else {
+      console.log('ok');
+      mongoose.disconnect();
+    }
+  });
 });
 
-user.save(function(err, user, affected) {
-  if (err) {
-    throw err;
-  } else {
-    User.findOne({username: 'Tester'}, function(err, tester) {
-      console.log(tester);
-    });
-  }
-});
+
+
+
 
 // var schema = mongoose.Schema({
 //   name: String
